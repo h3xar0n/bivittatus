599c1e219d58ecdc463e55fb75ddb6a68c567ee2
root <aron@eideldev.com>
2020-05-22 20:38:27 -0400

    remove log

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -42,21 +42,3 @@ options = get_target()
 scan_target = scan(options.target)
 
 print_scan(scan_target)
-
-# Errors:
-#
-# root@kali:~/PycharmProjects# pip3 install scapy-python3
-# Requirement already satisfied: scapy-python3 in /usr/local/lib/python3.7/dist-packages (0.26)
-# root@kali:~/PycharmProjects# python3 network_scanner/network_scanner.py -t 192.168.124.2/24
-#
-#         PIP package scapy-python3 used to provide scapy3k, which was a fork from scapy implementing python3 compatibility since 2016. This package was included in some of the Linux distros under name of python3-scapy. Starting from scapy version 2.4 (released in March, 2018) mainstream scapy supports python3. To reduce any confusion scapy3k was renamed to kamene.
-# You should use either pip package kamene for scapy3k (see http://github.com/phaethon/kamene for differences in use) or mainstream scapy (pip package scapy, http://github.com/secdev/scapy).
-#
-# Traceback (most recent call last):
-#   File "network_scanner/network_scanner.py", line 3, in <module>
-#     import scapy.all as scapy
-#   File "/usr/local/lib/python3.7/dist-packages/scapy/all.py", line 5, in <module>
-#     raise Exception(msg)
-# Exception:
-#         PIP package scapy-python3 used to provide scapy3k, which was a fork from scapy implementing python3 compatibility since 2016. This package was included in some of the Linux distros under name of python3-scapy. Starting from scapy version 2.4 (released in March, 2018) mainstream scapy supports python3. To reduce any confusion scapy3k was renamed to kamene.
-# You should use either pip package kamene for scapy3k (see http://github.com/phaethon/kamene for differences in use) or mainstream scapy (pip package scapy, http://github.com/secdev/scapy).
